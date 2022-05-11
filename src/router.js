157ae5e5ef5b9753231f25dc947d876720ba49ae
JaviMx <javimxmine@hotmail.com>
2022-05-11 17:58:55 -0500

    router modificado con las nuevas rutas

--- src/router.js
+++ src/router.js
@@ -15,7 +15,23 @@ const routes =  [
     path: "/add",
     name: "add-explorer",
     component: () => import("./components/AddExplorer")
-  }
+  },
+
+  {
+    path: "/mission",
+    name: "missionComander",
+    component: () => import("./components/MissionList")
+  },
+  {
+    path: "/mission/:id",
+    name: "mission-details",
+    component: () => import("./components/Mission")
+  },
+  {
+    path: "/NewMission",
+    name: "add-Mission",
+    component: () => import("./components/AddMission")
+  },
 ];
 const router = createRouter({
   history: createWebHistory(),
